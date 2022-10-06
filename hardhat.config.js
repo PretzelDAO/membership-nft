08b02157072a5c9fb6415ee000b6f7b702fd8a03
Totenfluch <github@cziegler.de>
2022-10-06 21:54:02 +0200

    add network

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,5 +7,11 @@ module.exports = {
   solidity: "0.8.17",
   etherscan: {
     apiKey: process.env.ETHERSCAN_KEY
-  }
+  },
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
 };
