11e925d61cb37356f2890eb9b6d7302adf4beb70
Totenfluch <github@cziegler.de>
2022-10-06 21:37:49 +0200

    add correct deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,19 +7,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const MembershipNFT = await hre.ethers.getContractFactory("MembershipNFT");
+  const membershipNFT = await MembershipNFT.deploy(1, 2022, 0xdAC17F958D2ee523a2206206994597C13D831ec7);
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  await membershipNFT.deployed();
 
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    'Deployed MembershipNFT Contract',
   );
 }
 
